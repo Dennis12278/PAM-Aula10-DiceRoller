4c542eedf983137b875b87fc876a392ef4928c56
Etec Prof. Horácio Augusto da Silveira <dennissilvageneroso@gmail.com>
2026-05-15 19:15:11 -0300

    Aula11

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿
+
+using DiceRoller.Models;
+
+namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+        
+        
+        
+        
+        private void SorteioButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int lados = Convert.ToInt16(LadosEntry.Text);
+                Dado dado = new Dado(lados);
+                ResultadoLabel.Text = Convert.ToString(dado.SorteiaNumero());
+
+                DadoImage.Source = $"dado{dado.NumeroSorteado}.png";
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            /*
+            //Sortear um número aleatório
+            Random aleatorio = new Random();
+            int numeroSorteado = aleatorio.Next(1,7);
+            // Exibe os valores na tela
+            resultadoLabel.Text = numeroSorteado.ToString();
+            DadoImage.Source = $"dado{numeroSorteado}.png";
+            
+            private void LadosEntry_TextChanged(object sender, TextChangedEventArgs e)
+            {
+            if (string.IsNullOrEmpty(LadosEntry.Text)) return;
+
+            if (!e.NewTextValue.All(char.IsDigit))
+            {
+                ((Entry)sender).Text = new string(e.NewTextValue.Where(char.IsDigit).ToArray());
+            }
+            */
+
+
+        }
+    }
+}
---
+++ DiceRoller/Models/Dado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceRoller.Models
+{
+    public class Dado
+    {
+        public Dado()
+        {
+            NumeroDeLados = 6;
+        }
+
+        public Dado(int numero)
+        {
+            NumeroDeLados = numero;
+        }
+
+        public int NumeroSorteado { get; set; }
+        public int NumeroDeLados{ get; set; }
+
+        
+        public int SorteiaNumero() {
+            Random aleatorio = new Random();
+            NumeroSorteado = aleatorio.Next(1, NumeroDeLados + 1 );
+            return NumeroSorteado;
+        }
+    }
+}
